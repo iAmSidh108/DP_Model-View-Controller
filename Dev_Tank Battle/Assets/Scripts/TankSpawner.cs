bcfce3c76b7219ef94115d016b5ac0864dd286e5
Siddharth Tiwari <sidh589@gmail.com>
2024-02-15 18:38:51 +0530

    Base Setup for Tank using MVC design Pattern.

---
+++ Dev_Tank Battle/Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+
+    public TankView tankView;
+    
+    void Start()
+    {
+        CreateTank();
+    }
+
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController tankController= new TankController(tankModel, tankView);
+    }
+
+    
+}
---
+++ Dev_Tank Battle/Assets/Scripts/TankView.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankView : MonoBehaviour
+{
+
+    private TankController tankController;
+    private float movement;
+    private float rotation;
+    public Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Movement();
+
+        if (movement != 0)
+        {
+            tankController.Move(movement, 30);
+        }
+
+        if (rotation != 0)
+        {
+            tankController.Rotate(rotation, 20);
+        }
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
+    }
+
+    public void SetTankController(TankController _tankController)
+    {
+        tankController = _tankController;
+    }
+
+    public Rigidbody GetRigidBody() 
+    {
+        return rb; 
+    }
+
+
+
+    
+}
